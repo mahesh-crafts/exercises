bed9d55863eb648519bad55286188b804271f293
mahesh-crafts <166956585+mahesh-crafts@users.noreply.github.com>
2024-04-25 14:57:49 -0700

    Update search_problem.py

--- search_problem.py
+++ search_problem.py
@@ -1,13 +1,10 @@
 import timeit
 
-def bruteforce_solution(input_size, target):
+def bruteforce_solution(input, input_size, target):
     """
     Given a list of numbers sorted in increasing order and a target number, find the target number in the list.
     If the target number is found in the list, return True. Otherwise, return False.
     """
-    start = 0
-    input = [i for i in range(start, input_size + 1)]
-    
     for i in range(input_size):
         if input[i] == target:
             print("Target found at index:", i)
@@ -17,7 +14,7 @@ def bruteforce_solution(input_size, target):
     return
 
 
-def optimal_solution(input_size, target):
+def optimal_solution(input, input_size, target):
     """
     Given a list of numbers sorted in increasing order and a target number, find the target number in the list.
     If the target number is found in the list, return True. Otherwise, return False.
@@ -27,14 +24,15 @@ def optimal_solution(input_size, target):
 
 input_size = 1000000000
 target = 999999999
+input = [i for i in range(0, input_size + 1)]
 
 print("Array size:", input_size)
 print("Target:", target)
 
 print("bruteforce_solution")
-print("Execution Time:", timeit.timeit(lambda: bruteforce_solution(input_size, target), number=1))
+print("Execution Time:", timeit.timeit(lambda: bruteforce_solution(input, input_size, target), number=1))
 
 print("optimal_solution")
-print("Execution Time:", timeit.timeit(lambda: optimal_solution(input_size, target), number=1))
+print("Execution Time:", timeit.timeit(lambda: optimal_solution(input, input_size, target), number=1))
 
 print("-" * 30)
