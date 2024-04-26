461bd81da85ef505e900ca724ec771c6e51c32d0
mahesh-crafts <166956585+mahesh-crafts@users.noreply.github.com>
2024-04-26 14:26:06 -0700

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+"""
+Problem:
+
+You are given an array of integers nums and an integer target. Write a function two_sum(nums, target) that returns the indices of the two numbers such that they add up to the target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
+"""
+def two_sum(nums, target):
+  pass
+
+
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))  # Output: [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
